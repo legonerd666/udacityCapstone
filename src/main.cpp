d756b2c945d25eaa4ee66022ee22c6daa7b1f33c
Liam <chipman.liam@gmail.com>
2022-08-10 15:34:54 -0300

    added a short introduction to the mainpage of the docs and the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,17 @@
 #include <memory>
 #include <ctime>
 
+/**
+ * \mainpage
+ * @brief A program that creates a custom Pathfinder 1e Character Sheet
+ *
+ * Uses the console to gather info from the user to populate a character sheet for Pathfinder 1e and then prints the fully created character sheet to the console.
+ *
+ * Good places to start:
+ * character
+ * creator
+ */
+
 int main()
 {
     srand(time(0));
